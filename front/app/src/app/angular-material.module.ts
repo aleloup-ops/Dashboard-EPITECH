236e33b4c07a15a1ffb50bb9b12f6929f78bd428
alexandreMegel <alexandre.megel@epitech.eu>
2020-11-23 16:36:47 +0100

    Add style for dashboard

--- front/app/src/app/angular-material.module.ts
+++ front/app/src/app/angular-material.module.ts
@@ -6,16 +6,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
 
 const materialModules = [
     MatIconModule,
     MatInputModule,
     MatSliderModule,
     MatButtonModule,
+    MatSidenavModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
--- front/app/src/app/app.component.ts
+++ front/app/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,21 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
     title = 'app';
+
+    constructor (public authService: AuthService, public router: Router) { }
+
+    /**
+     * 
+     */
+    redirectHome () {
+        if (this.authService.isLoggedIn === true)
+            this.router.navigate(['dashboard']);
+    }
+
+    /**
+     * 
+     */
+    logout () {
+        this.authService.SignOut();
+    }
 }
